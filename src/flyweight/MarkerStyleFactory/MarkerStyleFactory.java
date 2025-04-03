833e588795fd521eb2a72e20f93ed842226051d1
Aisultan779 <aisultanaidarkul0102@gmail.com>
2025-04-03 16:33:02 +0500

    add

---
+++ src/flyweight/MarkerStyleFactory/MarkerStyleFactory.java
@@ -0,0 +1,22 @@
+package flyweight;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MarkerStyleFactory {
+    private static final Map<String, MarkerStyle> styles = new HashMap<>();
+
+    public static MarkerStyle getStyle(String icon, String color, String labelStyle) {
+        String key = icon + "_" + color + "_" + labelStyle;
+        if (!styles.containsKey(key)) {
+            MarkerStyle style = new MarkerStyle(icon, color, labelStyle);
+            styles.put(key, style);
+            System.out.println("Создан новый стиль маркера: " + style);
+        }
+        return styles.get(key);
+    }
+
+    public static int getTotalStyles() {
+        return styles.size();
+    }
+}
+

f135e985480411ae0d27e5eeb5e62049495f03d8
Aisultan779 <aisultanaidarkul0102@gmail.com>
2025-04-03 16:31:35 +0500

    add

---
+++ src/flyweight/MapMarker/MapMarker.java
@@ -0,0 +1,16 @@
+package flyweight;
+public class MapMarker {
+    private final int x;
+    private final int y;
+    private final MarkerStyle style;
+
+    public MapMarker(int x, int y, String icon, String color, String labelStyle) {
+        this.x = x;
+        this.y = y;
+        this.style = MarkerStyleFactory.getStyle(icon, color, labelStyle);
+    }
+
+    public void display() {
+        System.out.println("Маркeр в точке (" + x + ", " + y + ") с стилем [" + style + "]");
+    }
+}
